ca83c9b487d1f3085c038f353c0dcc4155a9ffea
Evan Derby <somekidwithhtml@gmail.com>
2016-08-17 12:18:51 -0600

    FIRST EDYSH IS FIYAH

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,28 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+SOUNDCLOUD_EMBED_CONST = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=URL_GOES_HERE&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'
+
+$(document).ready(function() {
+  get_tracks();
+  $('#refresh-button').click(get_tracks);
+});
+
+function get_tracks() {
+  $.ajax('/tracks', {
+    success:function(data) {
+      $('#tracks-box').text("");
+      for (var i=0;i<data.length;i++) {
+        show_track(data[i])
+      }
+    },
+    beforeSend:function() {
+      $('#tracks-box').text("LOADING A NEW DOPE MIXTAPE FAM");
+    }
+  });
+}
+
+function show_track(track_url) {
+  $('#tracks-box').append(SOUNDCLOUD_EMBED_CONST.replace("URL_GOES_HERE", encodeURI(track_url)));
+}
---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,23 @@
+class StaticController < ApplicationController
+  def index
+  end
+
+  def fetch_tracks(client)
+    tracks = client.get('/tracks', :limit => 100, :tags => "hip-hop")
+    return tracks
+  end
+
+  def gen_rands(tracks)
+    return 10.times.map { tracks[Random.rand(100)] }
+  end
+
+  def tracks
+    client = SoundCloud.new(:client_id => ENV['CLIENT_ID'], :use_ssl => false)
+    rand_tracks = gen_rands(fetch_tracks(client))
+    while rand_tracks.include? nil
+      rand_tracks = gen_rands(fetch_tracks(client))
+    end
+    tracks_urls = rand_tracks.map { |t| t["permalink_url"] }
+    render :json => tracks_urls
+  end
+end
